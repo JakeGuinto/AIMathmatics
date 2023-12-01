722e17afdd531d761d4cc3854a59fc2d74e093b3
JakeGuinto <116121157+JakeGuinto@users.noreply.github.com>
2023-12-01 13:49:55 -0800

    Move it
    coded self-guided goal movement

---
+++ Points2D/Assets/Move.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public Transform goal;
+    public float accuracy = 0.01f;
+    public float speed = 2.0f;
+
+    void Start() 
+    {
+        
+    }
+
+    void LateUpdate() 
+    {
+        Vector3 direction = goal.position - this.transform.position;
+        if(direction.magnitude > accuracy)
+            this.transform.Translate(direction.normalized * speed * Time.deltaTime);
+    }
+}
