a4be69fe672f4fb1ac27c4c9fa44cdcd106d38b8
JakeGuinto <116121157+JakeGuinto@users.noreply.github.com>
2023-12-13 13:52:51 -0800

    Adam
    Made two adam models walk at diffrent times

--- Points2D/Assets/Move.cs
+++ Points2D/Assets/Move.cs
@@ -1,21 +1,23 @@
 ﻿using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class Move : MonoBehaviour {
-
+public class Move : MonoBehaviour 
+{
     public Transform goal;
     public float accuracy = 0.01f;
     public float speed = 2.0f;
 
     void Start() 
     {
-        
+        this.transform.LookAt(goal.position);
     }
 
     void LateUpdate() 
     {
+        this.transform.LookAt(goal.position);
         Vector3 direction = goal.position - this.transform.position;
-        if(direction.magnitude > accuracy)
-            this.transform.Translate(direction.normalized * speed * Time.deltaTime);
+        Debug.DrawRay(this.transform.position,direction,Color.red);
+        if (direction.magnitude > accuracy)
+            this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
     }
 }
